d000fa30f8cb4c88eb8f95d80ec62f9a48c85461
Nathan Wilcox <nathan@z.cash>
2016-12-16 09:22:46 -0800

    Switch from ``metricsui=0`` to ``showmetrics=0``.

--- mainnetfarm/init.py
+++ mainnetfarm/init.py
@@ -49,7 +49,7 @@ def init_node(nodedir, nodecount, nodenum, nodename, testnet):
           .rstrip('='))
         w('gen={}', 1 if nodenum == 0 else 0)
         w('debug=pow')
-        w('metricsui=0')
+        w('showmetrics=0')
 
         if testnet:
             w('testnet=1')
